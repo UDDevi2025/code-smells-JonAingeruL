c9fb8443d8a11244acfd1081f8a5ac461a6546d3
JonAingeruL <j.lobato@opendeusto.es>
2025-04-10 09:35:06 +0200

    solucion algunos smells

--- Assets/Scripts/FloorColision.cs
+++ Assets/Scripts/FloorColision.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JB : MonoBehaviour
+public class FloorColision : MonoBehaviour
 {
 
     public Rigidbody playerRigidbody;
